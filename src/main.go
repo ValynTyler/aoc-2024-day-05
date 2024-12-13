bb4d85410180d5a57f1832792cefd306d8278679
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2024-12-13 16:28:32 +0200

    feat: reading file lines

--- src/main.go
+++ src/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"valyntyler.com/aoc-2024-day-05/input"
 )
 
 func main()  {
-  fmt.Println(string(input.Bytes))
+  var s string = string(input.Bytes)
+  var result string = strings.Trim(s, " \n")
+  var lines []string = strings.Split(result, "\n")
+
+  for _, line := range lines {
+    fmt.Println(line)
+  }
 }
